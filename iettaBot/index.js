8042c6abeae93baeed5639b4cbc1e3bd9396334d
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-02-04 18:47:56 +0500

    cant setup webhooktoken

--- iettaBot/index.js
+++ iettaBot/index.js
@@ -4,11 +4,10 @@ import config from './config.json' assert { type: "json" };
 (async () => {
     try {
         //This just a structure, need to complete this all
-        const port = 3000;
         const you = '106372709038686';
-        const yourtoken = 'EABN0xF0imQ0BACRZBkFZBkQ48AP9RFaZCMKSmD9ygFZBPdiWNiDhrRZBO21aZAxmzyL0JDylAtLLPZCktwdIHGfFlInHAFMHZALNlbNY4L1EtQyKltaHjvrve6GSPiQoVR6sv1sgY3oaPZBhWnQC3KMXYGgsHQXgY31InZCLqfHj8EiVyQJ1rrM5sjLXodyayuJqwcu6PiUXjHDAZDZD';
+        const yourtoken = '';
         const yourHomie = '923347756188';
-        const webhookVerifyToken = 'EABN0xF0imQ0BACRZBkFZBkQ48AP9RFaZCMKSmD9ygFZBPdiWNiDhrRZBO21aZAxmzyL0JDylAtLLPZCktwdIHGfFlInHAFMHZALNlbNY4L1EtQyKltaHjvrve6GSPiQoVR6sv1sgY3oaPZBhWnQC3KMXYGgsHQXgY31InZCLqfHj8EiVyQJ1rrM5sjLXodyayuJqwcu6PiUXjHDAZDZD';
+        const webhookVerifyToken = '';
         console.log('hello');
         const iettaBot = createBot(you, yourtoken);
 
@@ -16,7 +15,6 @@ import config from './config.json' assert { type: "json" };
 
         await iettaBot.startExpressServer({
             webhookVerifyToken,
-            port,
         });
 
         iettaBot.on('message', async (msg) => {
